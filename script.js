1dca83b22e54cb08db5cce5fbae5bac19c580dda
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-14 12:22:45 -0700

    feat: Update to CPU choice pseudocode

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    // create a new integer variable for a random number & set it to a random number between 0 and 2
-    // IF the variable equals 0, return "rock"
-    // IF the variable equals 1, return "paper"
-    // IF the variable equals 0, return "scissors"
+    // create a new integer variable for a random number & set it to a random number between 1 and 3
+    // IF the variable equals 1, return "rock"
+    // IF the variable equals 2, return "paper"
+    // IF the variable equals 3, return "scissors"
 }
