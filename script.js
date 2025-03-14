fffd378c3ffa903daa8e121386d2d02ab7055796
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-14 12:16:11 -0700

    feat: Computer choice pseudocode

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    // create a new integer variable for a random number & set it to a random number between 0 and 2
+    // IF the variable equals 0, return "rock"
+    // IF the variable equals 1, return "paper"
+    // IF the variable equals 0, return "scissors"
+}
