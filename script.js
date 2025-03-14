c862063ed1fd710e8fec2a2f7717cf2f6acda7c9
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-03-14 12:35:06 -0700

    feat: CPU choice logic

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function getComputerChoice() {
     // create a new integer variable for a random number & set it to a random number between 1 and 3
+    let randomNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
     // IF the variable equals 1, return "rock"
-    // IF the variable equals 2, return "paper"
-    // IF the variable equals 3, return "scissors"
+    if (randomNum === 1) {
+        return "rock";
+    } else if (randomNum === 2) { // IF the variable equals 2, return "paper"
+        return "paper";
+    } else { // IF the variable equals 3, return "scissors"
+        return "scissors";
+    }
 }
